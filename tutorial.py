556f43e17bef2c1f75ae39a56b869b7f046e804a
Kevin Robinson <kevin.robinson@brandstreet.com>
2016-08-11 19:51:20 +0000

    Updated index.html

---
+++ tutorial.py
@@ -0,0 +1,15 @@
+print("Hi, welcome to my python script")
+
+name = "kevin"
+
+age = str(3 * 10)
+
+print("My name is" + ' ' + name + ' ' + "I am " + age + ' ' + "years old")
+
+<form method='post'>
+    <input type='text' name='num1' />
+    <input type='text' name='num1' />
+    <input type='submit' value='add' />
+</form>
+  
+            
